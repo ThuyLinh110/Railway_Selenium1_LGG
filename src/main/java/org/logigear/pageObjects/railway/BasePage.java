d0a533f343be044d063231f554b5700f36d1434c
ThuyLinh110 <thuylinh1102001@gmail.com>
2022-11-29 10:41:40 +0700

    basePage

---
+++ src/main/java/org/logigear/pageObjects/railway/BasePage.java
@@ -0,0 +1,45 @@
+package org.logigear.pageObjects.railway;
+
+import org.logigear.common.WebElementManager;
+import org.logigear.constant.Constant;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class BasePage {
+    //Locator of tabs
+    private String tabName = "//div[@id ='menu']//span[text()='%s']/..";
+    private By lblWelcome = By.xpath("//div[@class = 'account']//strong");
+    private By lblNamePage = By.xpath("//h1[@align='center']");
+
+    //Web Elements
+    protected WebElement getTab(String tab) {
+        return Constant.WEBDRIVER.findElement(By.xpath(String.format(tabName, tab)));
+    }
+
+    protected WebElement getMsgWelcome() {
+        return Constant.WEBDRIVER.findElement(lblWelcome);
+    }
+
+    protected WebElement getLblNamePage() {
+        return Constant.WEBDRIVER.findElement(lblNamePage);
+    }
+
+    public String getWelcomeMessage() {
+        return getMsgWelcome().getText();
+    }
+
+    public void clickTab(String tab) {
+        WebElementManager.clickToElement(getTab(tab));
+    }
+
+    public boolean checkTabDisplay(String tabName) {
+        WebElement tab = getTab(tabName);
+        if (tab != null) return true;
+        return false;
+    }
+
+    public String getNameTitlePage() {
+        return getLblNamePage().getText();
+    }
+
+}
